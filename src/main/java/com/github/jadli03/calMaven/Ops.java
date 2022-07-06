48cfa3511dddbedd515a88a73f0d11fded0bb6df
jadli03 <surajjadli@yahoo.com>
2022-07-06 10:12:52 +0530

    pushing maven project

---
+++ src/main/java/com/github/jadli03/calMaven/Ops.java
@@ -0,0 +1,24 @@
+package com.github.jadli03.calMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class Ops 
+{
+    	public int add(int a, int b) {
+    		return a+b;
+    	}
+    	
+    	public int sub(int a, int b) {
+    		return a-b;
+    	}
+    	
+    	public int mul(int a, int b) {
+    		return a*b;
+    	}
+    	
+    	public int div(int a, int b) {
+    		return a/b;
+    	}
+}
---
+++ src/test/java/com/github/jadli03/calMaven/OpsTest.java
@@ -0,0 +1,20 @@
+package com.github.jadli03.calMaven;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class OpsTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
